42b42afea4cc8c7f01f6845bc6a7a652ce027d79
ape <485424725@qq.com>
2022-07-29 20:08:10 +0800

    【作者】小猪 【功能】实例

---
+++ src/main/java/com/xxx/boot/bean/Dog.java
@@ -0,0 +1,12 @@
+package com.xxx.boot.bean;
+
+/**
+ * @classname：Dog
+ * @author：hejunhui
+ * @version：1.0
+ * @Description:TODO
+ * @date：2022/7/29-17:53
+ * @since:jdk1.8
+ */
+public class Dog {
+}
